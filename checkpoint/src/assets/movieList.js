4de4cb273b446a12b3d7e18c46726a1abb70b288
David Agbemuko <davidagbemuko.ad@gmail.com>
2025-02-25 15:23:47 +0000

    put in place a wikipedia reference for all movies

--- checkpoint/src/assets/movieList.js
+++ checkpoint/src/assets/movieList.js
@@ -3,31 +3,36 @@ const movieList =  [
       "title": "Inception",
       "description": "A thief who enters the dreams of others to steal secrets from their subconscious.",
       "posterURL": "/inception.jpg",
-      "rating": 8.8
+      "rating": 8.8,
+      "wiki": "https://en.wikipedia.org/wiki/Inception"
     },
     {
       "title": "The Dark Knight",
       "description": "Batman faces the Joker, a criminal mastermind causing chaos in Gotham.",
       "posterURL": "/dark-knight.jpg",
-      "rating": 9.0
+      "rating": 9.0,
+      "wiki":"https://en.wikipedia.org/wiki/The_Dark_Knight"
     },
     {
       "title": "Interstellar",
       "description": "A team of explorers travels through a wormhole in space to save humanity.",
       "posterURL": "/interstellar.jpg",
-      "rating": 8.6
+      "rating": 8.6,
+      "wiki":"https://en.wikipedia.org/wiki/Interstellar_(film)"
     },
     {
       "title": "The Matrix",
       "description": "A hacker discovers the true nature of reality and his role in the war against its controllers.",
       "posterURL": "/matrix.jpg",
-      "rating": 8.7
+      "rating": 8.7,
+      "wiki": "https://en.wikipedia.org/wiki/The_Matrix"
     },
     {
       "title": "Gladiator",
       "description": "A betrayed Roman general seeks revenge against the corrupt emperor who murdered his family.",
       "posterURL": "/gladiator.jpg",
-      "rating": 8.5
+      "rating": 8.5,
+      "wiki": "https://en.wikipedia.org/wiki/Gladiator_(2000_film)"
     }
   ]
   
--- checkpoint/src/components/MovieCard.jsx
+++ checkpoint/src/components/MovieCard.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import movieList from '../assets/movieList'
 
-const MovieCard = () => {
+const MovieCard = ({title, description, posterURL, rating, wiki}) => {
 
     return (
             <div class="card" style={{width: "18rem"}}>
-                <img src={movieList[0]['posterURL']} class="card-img-top" alt="Movie Poster" />
+                <img src={posterURL} class="card-img-top" alt="Movie Poster" />
                 <div class="card-body">
-                    <h5 class="card-title">Movie Title</h5>
-                    <p class="card-text">Short description of the movie goes here.</p>
+                    <h5 class="card-title">{title}</h5>
+                    <p class="card-text">{description}</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><strong>Rating:</strong> ★ 8.5</li>
+                    <li class="list-group-item"><strong>Rating:</strong> ★ {rating}</li>
                 </ul>
                 <div class="card-body">
-                    <a href="#" class="card-link">More Info</a>
+                    <a href={wiki} class="card-link">More Info</a>
                 </div>
             </div>
 
