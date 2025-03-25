be703e49e0336cda5868720a5930780e25b762a4
Apoti-Eri117 <apotieri117@gmail.com>
2025-03-25 16:25:18 +0000

    added base to vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/react-hooks-checkpoint/',
   server: {
     allowedHosts: [
       "5173-arkarch-reacthookscheck-bazgoik6cpm.ws-eu118.gitpod.io" // Add your Gitpod host
