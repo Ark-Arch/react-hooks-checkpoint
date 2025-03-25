0225be541f07d2b47a6433871ce5b3da81262a42
Apoti-Eri117 <apotieri117@gmail.com>
2025-03-25 17:21:35 +0000

    fix images

--- src/assets/movieList.js
+++ src/assets/movieList.js
@@ -2,35 +2,35 @@ const movieList =  [
     {
       "title": "Inception",
       "description": "A thief who enters the dreams of others to steal secrets from their subconscious.",
-      "posterURL": "/inception.jpg",
+      "posterURL": "/react-hooks-checkpoint/inception.jpg",
       "rating": 8.8,
       "wiki": "https://en.wikipedia.org/wiki/Inception"
     },
     {
       "title": "The Dark Knight",
       "description": "Batman faces the Joker, a criminal mastermind causing chaos in Gotham.",
-      "posterURL": "/dark-knight.jpg",
+      "posterURL": "/react-hooks-checkpoint/dark-knight.jpg",
       "rating": 9.0,
       "wiki":"https://en.wikipedia.org/wiki/The_Dark_Knight"
     },
     {
       "title": "Interstellar",
       "description": "A team of explorers travels through a wormhole in space to save humanity.",
-      "posterURL": "/intersteller.jpg",
+      "posterURL": "/react-hooks-checkpoint/intersteller.jpg",
       "rating": 8.6,
       "wiki":"https://en.wikipedia.org/wiki/Interstellar_(film)"
     },
     {
       "title": "The Matrix",
       "description": "A hacker discovers the true nature of reality and his role in the war against its controllers.",
-      "posterURL": "/matrix.jpg",
+      "posterURL": "/react-hooks-checkpoint/matrix.jpg",
       "rating": 8.7,
       "wiki": "https://en.wikipedia.org/wiki/The_Matrix"
     },
     {
       "title": "Gladiator",
       "description": "A betrayed Roman general seeks revenge against the corrupt emperor who murdered his family.",
-      "posterURL": "/gladiator.jpg",
+      "posterURL": "/react-hooks-checkpoint/gladiator.jpg",
       "rating": 8.5,
       "wiki": "https://en.wikipedia.org/wiki/Gladiator_(2000_film)"
     }
