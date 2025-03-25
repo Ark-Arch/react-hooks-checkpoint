dae1ee0ec66ddc919d621c324cbd7fc9a747d326
Apoti-Eri117 <apotieri117@gmail.com>
2025-03-25 17:10:23 +0000

    image fix

--- src/assets/movieList.js
+++ src/assets/movieList.js
@@ -2,35 +2,35 @@ const movieList =  [
     {
       "title": "Inception",
       "description": "A thief who enters the dreams of others to steal secrets from their subconscious.",
-      "posterURL": "/react-api-checkpoint/inception.jpg",
+      "posterURL": "/inception.jpg",
       "rating": 8.8,
       "wiki": "https://en.wikipedia.org/wiki/Inception"
     },
     {
       "title": "The Dark Knight",
       "description": "Batman faces the Joker, a criminal mastermind causing chaos in Gotham.",
-      "posterURL": "/react-api-checkpoint/dark-knight.jpg",
+      "posterURL": "/dark-knight.jpg",
       "rating": 9.0,
       "wiki":"https://en.wikipedia.org/wiki/The_Dark_Knight"
     },
     {
       "title": "Interstellar",
       "description": "A team of explorers travels through a wormhole in space to save humanity.",
-      "posterURL": "/react-api-checkpoint/intersteller.jpg",
+      "posterURL": "/intersteller.jpg",
       "rating": 8.6,
       "wiki":"https://en.wikipedia.org/wiki/Interstellar_(film)"
     },
     {
       "title": "The Matrix",
       "description": "A hacker discovers the true nature of reality and his role in the war against its controllers.",
-      "posterURL": "/react-api-checkpoint/matrix.jpg",
+      "posterURL": "/matrix.jpg",
       "rating": 8.7,
       "wiki": "https://en.wikipedia.org/wiki/The_Matrix"
     },
     {
       "title": "Gladiator",
       "description": "A betrayed Roman general seeks revenge against the corrupt emperor who murdered his family.",
-      "posterURL": "/react-api-checkpoint/gladiator.jpg",
+      "posterURL": "/gladiator.jpg",
       "rating": 8.5,
       "wiki": "https://en.wikipedia.org/wiki/Gladiator_(2000_film)"
     }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -21,29 +21,29 @@ const AddMovie = ({handleAddNewMovie}) => {
 
     return (
         <>
-            <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addMovieModal">
+            <button className="btn btn-success" data-bs-toggle="modal" data-bs-target="#addMovieModal">
             Add Movie
             </button>
 
-            <div class="modal fade" id="addMovieModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="modalLabel">Add a New Movie</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal fade" id="addMovieModal" tabIndex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="modalLabel">Add a New Movie</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
 
-                        <div class="modal-body">
-                            <input type="text" class="form-control mb-2" placeholder="Movie Title" id="movieTitle" ref={titleRef}/>
-                            <input type="text" class="form-control mb-2" placeholder="Description" id="movieDescription" ref={descriptionRef}/>
-                            <input type="text" class="form-control mb-2" placeholder="Poster URL" id="moviePoster" ref={posterRef}/>
-                            <input type="number" class="form-control mb-2" placeholder="Rating" id="movieRating" min="1" max="10" ref={ratingRef}/>
-                            <input type="text" class="form-control-mb-2" placeholder="Wiki page" id="movieWiki" ref={wikiRef}/>
+                        <div className="modal-body">
+                            <input type="text" className="form-control mb-2" placeholder="Movie Title" id="movieTitle" ref={titleRef}/>
+                            <input type="text" className="form-control mb-2" placeholder="Description" id="movieDescription" ref={descriptionRef}/>
+                            <input type="text" className="form-control mb-2" placeholder="Poster URL" id="moviePoster" ref={posterRef}/>
+                            <input type="number" className="form-control mb-2" placeholder="Rating" id="movieRating" min="1" max="10" ref={ratingRef}/>
+                            <input type="text" className="form-control-mb-2" placeholder="Wiki page" id="movieWiki" ref={wikiRef}/>
                         </div>
 
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={handleAddingNew}>Save Movie</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={handleAddingNew}>Save Movie</button>
                         </div>
 
                     </div>
